236fa813df0c639e7817f39f41d65514d010fad4
Official21A <officialamirhossein21@gmail.com>
2020-12-01 08:41:48 +0330

    begin

---
+++ 1.Rounding/round.py
@@ -0,0 +1,16 @@
+# Problem 1:
+# You want to round a floting point number to a fixed number of decimal places
+
+# For simple rounding just do round(value, ndigits)
+
+# Simple float
+float_num = 13.42575
+
+print(round(float_num, 1))
+print(round(float_num, 2))
+print(round(float_num, 3))
+
+# Round to decimal
+a = round(float_num, 0)
+print(a)
+
