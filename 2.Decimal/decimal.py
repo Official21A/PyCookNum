6122b238a9e7436a76adb2b0c31b49f53a4c94f0
Official21A <officialamirhossein21@gmail.com>
2020-12-02 08:03:09 +0330

    fine decimal

--- 2.Decimal/decimal.py
+++ 2.Decimal/decimal.py
@@ -11,6 +11,7 @@ print(a + b) # 13.5000000001
 
 # to fix this we can work with Decimal module.
 from decimal import Decimal
+
 a = Decimal("10.1")
 b = Decimal("3.4")
 c = a + b
@@ -18,3 +19,17 @@ c = a + b
 print(c) # 13.5
 print(x == Decimal(13.5)) # True
 
+
+# Another feature of Decimals is that it allows you to control different 
+# aspects of calculations.
+# To do this you create a local context and change its settings.
+from decimal import localcontext
+
+d = Decimal("6.4")
+e = Decimal("12.5")
+
+print(e / d) # 1.953125
+
+with localcontext() as ctx:
+	ctx.prec = 3
+	print(e / d) # 1.953
